a350357b176bc3f5d31a0584cff6df726a52c784
lucascdlima <lucascdlima@gmail.com>
2020-09-04 12:18:33 -0300

    Updates standard deviation values of odometry and landmarks extraction measures.

--- examples/particle_filter_sim.py
+++ examples/particle_filter_sim.py
@@ -6,9 +6,9 @@ if __name__ == "__main__":
    np = pfilter.np
    fig1, ax1 = plt.subplots()
    x0 = np.array((0.0,0.0,0.0))
-   odom_variance = [0.6, 0.4, 0.4, 0.4]
-   landmark_variance = [0.2, 0.2, 0.1]
+   odom_std = [1.8, 0.5, 0.05, 0.02]
+   landmark_std = [0.2, 0.2]
    M_particles= 500
    T = 20
    dt = 0.1
-   pfilter.particle_filter_simulation(x0, odom_variance, landmark_variance, M_particles, T, dt, "", ax1, plt)
+   pfilter.particle_filter_simulation(x0, odom_std, landmark_std, M_particles, T, dt, "", ax1, plt)
