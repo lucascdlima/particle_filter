1abb7c16d83bda0997ed086b4c62902ba92c599e
lucascdlima <lucascdlima@gmail.com>
2020-10-21 20:13:04 -0300

    Changes standard deviation values.

--- examples/particle_filter_sim.py
+++ examples/particle_filter_sim.py
@@ -6,8 +6,13 @@ if __name__ == "__main__":
    np = pfilter.np
    fig1, ax1 = plt.subplots()
    x0 = np.array((0.0,0.0,0.0))
-   odom_std = [1.8, 0.5, 0.05, 0.02]
-   landmark_std = [0.2, 0.2]
+   #odom_std = [1.8, 0.5, 0.05, 0.02]
+   #landmark_std = [0.2, 0.2]
+
+   odom_std = [0.1, 0.01, 0.02, 0.01]
+   #landmark_std = [0.05, 0.05]
+   landmark_std = [0.1, 0.1]
+
    M_particles= 500
    T = 20
    dt = 0.1
